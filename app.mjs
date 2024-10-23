254f9b0a0ecf717a139268369753d8f66208eae2
Muhammad Firmansyah Rifai <firmansyahm9@gmail.com>
2024-10-23 10:29:20 +0700

    add Get and Create Todo List

--- app.mjs
+++ app.mjs
@@ -1,19 +1,17 @@
 import http from "http";
-import {TodolistService} from "./todolist-service.mjs";
+import {TodoListService} from "./todolist-service.mjs";
 
-const service = new TodolistService();
+const service = new TodoListService();
 const server = http.createServer((request, response) => {
+    // response.write('Todolist API');
+    // response.end();
 
     response.setHeader("Content-Type", "application/json");
 
-    if (request.method === "GET") {
-        service.getTodoList(request, response);
-    } else if (request.method === "POST") {
+    if(request.method === 'GET' && request.url === '/v1/getTodoList') {
+        service.getTodolist(request, response);
+    } else if (request.method === 'POST' && request.url === '/v1/createTodo') {
         service.createTodo(request, response);
-    } else if (request.method === "PUT") {
-        service.updateTodo(request, response);
-    } else if (request.method === "DELETE") {
-        service.deleteTodo(request, response);
     }
 
 });
--- todolist-service.mjs
+++ todolist-service.mjs
@@ -1,57 +1,32 @@
-export class TodolistService {
+export class TodoListService {
 
-    todolist = ["Programmer", "Zaman", "Now"];
+    todolist = [];
 
     getJsonTodoList() {
         return JSON.stringify({
             code: 200,
-            status: "OK",
-            data: this.todolist.map((value, index) => {
+            status: "Success get todo list", 
+            data: this.todolist.map((value,index) => {
                 return {
                     id: index,
                     todo: value
                 }
             })
         });
-    }
+    };
 
-    getTodoList(request, response) {
+    getTodolist(request,response) {
         response.write(this.getJsonTodoList());
-        response.end();
-    }
+        response.end();  
+    };
 
-    createTodo(request, response) {
+    createTodo (request,response) {
         request.addListener("data", (data) => {
             const body = JSON.parse(data.toString());
             this.todolist.push(body.todo);
 
             response.write(this.getJsonTodoList());
             response.end();
-        })
-    }
-
-    updateTodo(request, response) {
-        request.addListener("data", (data) => {
-            const body = JSON.parse(data.toString());
-            if (this.todolist[body.id]) {
-                this.todolist[body.id] = body.todo;
-            }
-
-            response.write(this.getJsonTodoList());
-            response.end();
-        })
-    }
-
-    deleteTodo(request, response){
-        request.addListener("data", (data) => {
-            const body = JSON.parse(data.toString());
-            if (this.todolist[body.id]) {
-                this.todolist.splice(body.id, 1);
-            }
-
-            response.write(this.getJsonTodoList());
-            response.end();
-        })
-    }
-
-}
+        });
+    };
+};
